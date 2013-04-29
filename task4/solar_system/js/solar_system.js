c4b57b87371abdd8af6ae4b656fb4e2e045847fa
am636e <bnazariy@gmail.com>
2013-04-29 16:51:52 +0300

    solar system without bells and whistels

--- task4/solar_system/js/solar_system.js
+++ task4/solar_system/js/solar_system.js
@@ -1,26 +1,8 @@
 var planets = document.getElementsByClassName( "planet" );
-document.getElementById( "sun" ).style.left = screen.width / 2 + 'px';
-document.getElementById( "sun" ).style.top = screen.height / 2 + 'px';
-/*(function( els )
-{
-	
-})( planets );*/
-/*(function ( elements )
-{
-	var x = 10;
-	var parentX = 0;
-	var parentY = 0;
-	for( var i = 0; i < elements.lenght; i ++ )
-	{
-		parentX = elements[ i ].parentNode.offsetLeft;
-		elements[ i ].style.marginLeft = 100 + 'px';
-		
-		x *= 2;
-	}
-})( planets );*/
+
 
 var angle = 0;
-var deltaAngle = 2 * Math.PI / 180;
+var deltaAngle = Math.PI / 180;
 function show( params )
 {
 	var show = 0;
@@ -30,23 +12,22 @@ function show( params )
 	}
 	alert( show );
 }
-function moveElement( element )
+function moveElement( element, radius )
 {
 	var parentX = element.parentNode.offsetLeft;
 	var parentY = element.parentNode.offsetTop;
 	var a = document.getElementById( "sun" );
-	//alert( a.offsetLeft + " " + a.offsetTop );
 	
-	var radius = element.offsetTop;//Math.abs( parentY - element.offfsetTop );
-	
-	var x = ( element.offsetLeft ) + radius * Math.cos( angle );
-	var y = ( element.offsetTop ) + radius * Math.sin( angle );
+	var x = element.parentNode.offsetWidth / 2 - 20+ radius * Math.cos( angle );
+	var y = element.parentNode.offsetHeight / 2 - 120 + radius * Math.sin( angle );
 	
 	element.style.left = x + 'px';
 	element.style.top = y + 'px';
 	
-	angle -= deltaAngle;
-	
+	angle -= deltaAngle;	
 }
 
-setInterval( "moveElement( planets[ 0 ] )", 100 );
+setInterval( "moveElement( planets[ 0 ], 75 )", 100 );
+setInterval( "moveElement( planets[ 1 ], 150 )", 100 );
+setInterval( "moveElement( planets[ 2 ], 30 )", 100 );
+setInterval( "moveElement( planets[ 3 ], 300 )", 100 );
