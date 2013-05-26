a0a6cc10df517070aa935e9f8c985b8e47656af7
AM636E <bnazariy@gmail.com>
2013-05-26 05:57:56 +0300

    Update element_creator.js

--- Builder/js/element_creator.js
+++ Builder/js/element_creator.js
@@ -5,10 +5,6 @@ var text = document.getElementById( "text" );
 var attr_name = document.getElementById( "attr_name" );
 var attr_value = document.getElementById( "attr_value" );
 
-function insertAfter(referenceNode, newNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-}
-
 function addElement( )
 {
 	var el = document.createElement( choosenEl );
@@ -20,5 +16,5 @@ function addElement( )
 	
 	el.setAttribute( attr_name.value, attr_value.value );
 
-	insertAfter( elHolder.firstChild, el );
+	elHolder.insertBefore( el, el.holder.nextSibling );
 }
